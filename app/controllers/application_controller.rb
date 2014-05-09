dcdc90c811122708babafc92083afbbb315ea22f
Scott O'Hara <scott@oharagroup.net>
2014-05-09 10:39:19 +1000

    Added global rescue_from handlers to respond with JSON error messages (#27)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,32 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 	before_action :authenticate_user
+	rescue_from StandardError, :with => :internal_error
+	rescue_from ActiveRecord::RecordInvalid, :with => :record_invalid
+	rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
 	def authenticate_user
 		render :text => "Invalid login and/or password", :status => :unauthorized unless authenticate_with_http_basic do |username, password|
 			username.eql?(ENV[:LOOT_USERNAME.to_s]) && password.eql?(ENV[:LOOT_PASSWORD.to_s])
 		end
 	end
+
+	def internal_error(exception)
+		p exception
+		render :json => exception.message, :status => :internal_server_error
+	end
+
+	def record_invalid(exception)
+		p exception
+		render :json => exception.record.errors.full_messages.join(', '), :status => :unprocessable_entity
+	end
+
+	def record_not_found(exception)
+		p exception
+		render :json => exception.message, :status => :not_found
+	end
 end
+
+
+
+
