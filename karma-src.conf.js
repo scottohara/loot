3fd4e24747c95ebbc7f7733c17ff9b4371c2275b
Scott O'Hara <scott@oharagroup.net>
2015-04-30 15:29:13 +1000

    Ignore protractor specs in karma config

--- karma-src.conf.js
+++ karma-src.conf.js
@@ -69,6 +69,7 @@ module.exports = function(config) {
 
 		// list of files to exclude
 		exclude: [
+			"spec/public/**/views/*.js"
 		],
 
 		// preprocess matching files before serving them to the browser
