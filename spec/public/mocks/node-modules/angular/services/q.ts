af34090fdb6ea8fd4a9759b47da577b45ddd1318
Scott O'Hara <scott@oharagroup.net>
2023-07-25 08:32:09 +1000

    Upgrade to typescript@5.1.6

--- spec/public/mocks/node-modules/angular/services/q.ts
+++ spec/public/mocks/node-modules/angular/services/q.ts
@@ -71,7 +71,7 @@ export default class QMockProvider implements Mock<QMock> {
 			qSuccess.resolve((success as PromiseMockConfig<T> | undefined)?.response ?? null);
 
 			// Auto-reject the error promise with the specified error response
-			qError.reject((error as PromiseMockConfig<T> | undefined)?.response ?? { data: "unsuccessful" });
+			qError.reject((error as PromiseMockConfig<T> | undefined)?.response ?? { data: "unsuccessful" as const });
 
 			// Configure the stub to return the appropriate promise based on the call arguments
 			if (undefined === success || (angular.isObject(success) && undefined === (success as PromiseMockConfig<T>).args)) {
