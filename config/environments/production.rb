2fb0d6d333f4e5cb40b97e713e29219eb9c89ecd
Scott O'Hara <scott@oharagroup.net>
2026-08-22 14:37:11 +1000

    Enable DNS rebinding protection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,11 +54,8 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  #
+  config.hosts = [/loot(test)?\.oharagroup\.net/]
+
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
